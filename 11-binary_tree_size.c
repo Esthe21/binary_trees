97f0cf35521e6995b8c89499a783530f7a4a4d84
Esthe21 <129193694+Esthe21@users.noreply.github.com>
2023-11-28 17:13:21 +0000

    Update 11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - Measures the size of a binary tree.
+ * binary_tree_size - Measures the size of a binary tree..
  * @tree: A pointer to the root node of the tree to measure the size of.
  *
  * Return: The size of the tree.
